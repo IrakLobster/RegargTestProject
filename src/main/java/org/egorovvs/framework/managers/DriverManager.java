6bf8eab63c8017e7c47a4b1b11600055b75eafaa
Vladislav Egorov <skillet2828@mail.ru>
2025-06-09 15:33:05 +0400

    и опять

--- src/main/java/org/egorovvs/framework/managers/DriverManager.java
+++ src/main/java/org/egorovvs/framework/managers/DriverManager.java
@@ -5,19 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverManager {
 
-<<<<<<< HEAD
     private WebDriver driver;
 
     private static DriverManager driverManager;
 
     private TestPropManager testPropManager = TestPropManager.getTestPropManager();
-=======
-    private  WebDriver driver;
-
-    private static DriverManager driverManager;
-
-    private  TestPropManager testPropManager = TestPropManager.getTestPropManager();
->>>>>>> a2a1b5512e184cdb97a584116b88bb32530a4589
 
     private DriverManager(){
 
