4515210a6ec1501cdc058aa18be51745ade5e312
Vladislav Egorov <skillet2828@mail.ru>
2025-06-09 15:40:47 +0400

    asdas

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -2,11 +2,9 @@ import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
-<<<<<<< HEAD
-=======
+
 import org.junit.platform.suite.api.Suite;
 
->>>>>>> a2a1b5512e184cdb97a584116b88bb32530a4589
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 
@@ -16,9 +14,5 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/scenario/cucumberTest.feature")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 public class CucumberRunner {
-<<<<<<< HEAD
-    System.out.ptintln("Goodbye world");
-=======
-    System.out.println("Hello world and good day");
->>>>>>> a2a1b5512e184cdb97a584116b88bb32530a4589
+
 }
